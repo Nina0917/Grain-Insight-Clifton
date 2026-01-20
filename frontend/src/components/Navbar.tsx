4213020836edb979b296d66c9810f9e333fdaf21
JiaxuanTong <50501028+JiaxuanTong@users.noreply.github.com>
2026-01-19 20:35:30 -0700

    re-formated

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -11,13 +11,34 @@ export default function Navbar() {
         {/* For small screen */}
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
           </div>
           <ul
             tabIndex={-1}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-            <li><button onClick={() => navigate("/documents")}>Documents</button></li>
-            {isAdmin() && (<li><button onClick={() => navigate("/users")}>Users</button></li>)}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <button onClick={() => navigate("/documents")}>Documents</button>
+            </li>
+            {isAdmin() && (
+              <li>
+                <button onClick={() => navigate("/users")}>Users</button>
+              </li>
+            )}
           </ul>
         </div>
         {/* Logo */}
@@ -29,20 +50,24 @@ export default function Navbar() {
         {/* Function buttons */}
 
         <ul className="menu menu-horizontal px-1 hidden lg:flex">
-
-          <li><button onClick={() => navigate("/documents")}>Documents</button></li>
-          {isAdmin() && (<li><button onClick={() => navigate("/users")}>Users</button></li>)}
+          <li>
+            <button onClick={() => navigate("/documents")}>Documents</button>
+          </li>
+          {isAdmin() && (
+            <li>
+              <button onClick={() => navigate("/users")}>Users</button>
+            </li>
+          )}
         </ul>
-        
       </div>
 
       {/* Navbar right: User Avatar and Dropdown Menu */}
       <div className="navbar-end">
-       <div className="dropdown dropdown-end">
-         <div
-          tabIndex={0}
-          role="button"
-          className="btn btn-ghost btn-circle avatar"
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
           >
             <div className="w-10 rounded-full">
               <img
--- frontend/src/pages/Documents.tsx
+++ frontend/src/pages/Documents.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import DocumentsTable from "../components/DocumentsTable";
 import { tokenManager } from "../utils/tokenManager";
-import Navbar from "../components/Navbar"; 
+import Navbar from "../components/Navbar";
 
 export default function Documents() {
   const [open, setOpen] = useState(false); // determines if the upload modal is open
